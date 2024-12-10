735a916f6bc826567c80ce433195caa0d84f0514
DESKTOP-9D2J144\certimeter <bilguun.batsaikhan@lutech.it>
2024-12-10 14:44:20 +0100

    refresh through bff

--- src/main/java/com/example/bff/service/BffService.java
+++ src/main/java/com/example/bff/service/BffService.java
@@ -3,6 +3,8 @@ package com.example.bff.service;
 import com.example.bff.exception.CustomClientException;
 import com.example.bff.resourcemodel.LoginRequest;
 import com.example.bff.resourcemodel.LoginResponse;
+import com.example.bff.resourcemodel.RefreshRequest;
+import com.example.bff.resourcemodel.RefreshResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -35,6 +37,24 @@ public class BffService {
             throw new CustomClientException(statusCode, errorMessage);
         } catch (ResourceAccessException e) {
             throw new CustomClientException(HttpStatus.SERVICE_UNAVAILABLE, "Login service is currently unavailable. Please try again later.");
+        }
+    }
+
+    public RefreshResponse refresh(String accessToken, RefreshRequest refreshRequest) {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setBearerAuth(accessToken);
+            HttpEntity<RefreshRequest> entity = new HttpEntity<>(refreshRequest, headers);
+
+            ResponseEntity<RefreshResponse> exchange = restTemplate.exchange(loginApiUrl + "/refresh", HttpMethod.POST, entity, RefreshResponse.class);
+            return exchange.getBody();
+        } catch (HttpClientErrorException e) {
+            String errorMessage = e.getResponseBodyAsString();
+            HttpStatusCode statusCode = e.getStatusCode();
+            throw new CustomClientException(statusCode, errorMessage);
+        } catch (ResourceAccessException e) {
+            throw new CustomClientException(HttpStatus.SERVICE_UNAVAILABLE, "Refresh service is currently unavailable. Please try again later.");
         }
     }
 }
