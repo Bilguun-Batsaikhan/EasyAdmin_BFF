4b8b8d7e505d60ca802b14668f679c4cb5aea872
DESKTOP-9D2J144\certimeter <bilguun.batsaikhan@lutech.it>
2025-01-14 18:02:34 +0100

    Added priority for filtering, I had forgotten to include before.

--- src/main/java/com/certimeter/bff/service/TicketService.java
+++ src/main/java/com/certimeter/bff/service/TicketService.java
@@ -177,6 +177,8 @@ public class TicketService {
         bffService.appendOptionalParam(urlBuilder, "ticketTypeMatchMode", ticketTypeMatchMode);
         bffService.appendOptionalParam(urlBuilder, "status", status);
         bffService.appendOptionalParam(urlBuilder, "statusMatchMode", statusMatchMode);
+        bffService.appendOptionalParam(urlBuilder, "priority", priority);
+        bffService.appendOptionalParam(urlBuilder, "priorityMatchMode", priorityMatchMode);
 
         return urlBuilder.toString();
     }
