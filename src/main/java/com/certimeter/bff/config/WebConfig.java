194e4cb8826a62c295e205855828945da5ccb6ed
Bilguun <bilguun.ing@gmail.com>
2025-05-17 19:28:19 +0200

    CORS error configuration

--- src/main/java/com/certimeter/bff/config/WebConfig.java
+++ src/main/java/com/certimeter/bff/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200", // local dev
+                                "https://smart-assets.netlify.app") // deployed frontend
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
